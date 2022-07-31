6c640978d1c7f4a6edbd579b10fa82d824e17eda
Kunj Manavadariya <manavadariyakunj@gmail.com>
2022-07-31 13:24:24 +0530

    Time: 7 ms (37.93%), Space: 8 MB (68.83%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -18,18 +18,6 @@ public:
             }
             freq.push_back(count);
         }
-        for(int i=0; i<freq.size(); i++){
-            cout<<freq[i]<<" ";
-        }
-        cout<<endl;
-        // for(int i=1; i<freq.size(); i++){
-        //     int temp;
-        //     if(freq[i]>freq[i-1]){
-        //     temp=freq[i];
-        //     freq[i]=freq[i-1];
-        //     freq[i-1]=temp;
-        //     }
-        // }
         sort(freq.begin(), freq.end());
         for(int i=1; i<freq.size(); i++){
             if(freq[i]==freq[i-1]){
