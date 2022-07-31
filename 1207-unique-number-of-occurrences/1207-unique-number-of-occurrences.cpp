dc98b3f19b5c29ab5a18871623adee89dd3e0f78
Kunj Manavadariya <manavadariyakunj@gmail.com>
2022-07-31 13:47:05 +0530

    Time: 6 ms (44.44%), Space: 8 MB (68.83%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -2,27 +2,9 @@ class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
         int arr1[2001] = {0};
-        // for(int i=0; i<arr.size(); i++){
-        //     int count=1;
-        //     if(arr[i]==1001){
-        //         continue;
-        //     }
-        //     for(int j=i+1; j<arr.size(); j++){
-        //         if(arr[j]==1001){
-        //          continue;   
-        //         }
-        //         if(arr[i]==arr[j]){
-        //             arr[j]=1001;
-        //             count++;
-        //         }
-        //     }
-        //     freq.push_back(count);
-        // }
         for(int i=0; i<arr.size(); i++){
             arr1[1000+arr[i]]++;
-            cout<<arr1[1000+arr[i]]<<" ";
         }
-        cout<<endl;
         sort(arr1,arr1+2001);
         for(int i=1; i<2001; i++){
             if(arr1[i] && arr1[i]==arr1[i-1]){
