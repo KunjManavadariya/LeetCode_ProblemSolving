f1a8200d641c66b41ae3c6becb04d24c12f58346
Kunj Manavadariya <manavadariyakunj@gmail.com>
2022-07-31 13:46:38 +0530

    Time: 4 ms (63.26%), Space: 7.9 MB (87.88%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -1,26 +1,31 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        vector<int> freq;
+        int arr1[2001] = {0};
+        // for(int i=0; i<arr.size(); i++){
+        //     int count=1;
+        //     if(arr[i]==1001){
+        //         continue;
+        //     }
+        //     for(int j=i+1; j<arr.size(); j++){
+        //         if(arr[j]==1001){
+        //          continue;   
+        //         }
+        //         if(arr[i]==arr[j]){
+        //             arr[j]=1001;
+        //             count++;
+        //         }
+        //     }
+        //     freq.push_back(count);
+        // }
         for(int i=0; i<arr.size(); i++){
-            int count=1;
-            if(arr[i]==1001){
-                continue;
-            }
-            for(int j=i+1; j<arr.size(); j++){
-                if(arr[j]==1001){
-                 continue;   
-                }
-                if(arr[i]==arr[j]){
-                    arr[j]=1001;
-                    count++;
-                }
-            }
-            freq.push_back(count);
+            arr1[1000+arr[i]]++;
+            cout<<arr1[1000+arr[i]]<<" ";
         }
-        sort(freq.begin(), freq.end());
-        for(int i=1; i<freq.size(); i++){
-            if(freq[i]==freq[i-1]){
+        cout<<endl;
+        sort(arr1,arr1+2001);
+        for(int i=1; i<2001; i++){
+            if(arr1[i] && arr1[i]==arr1[i-1]){
                 return false;
             }
         }
