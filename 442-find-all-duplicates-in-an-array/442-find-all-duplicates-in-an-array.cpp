8680f73a126da571d0195e018e4655881512691e
Kunj Manavadariya <manavadariyakunj@gmail.com>
2022-08-01 22:01:10 +0530

    Time: 335 ms (5.03%), Space: 33.6 MB (48.88%) - LeetHub

---
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> res;
+       sort(nums.begin(), nums.end());
+        for(int i=0; i<nums.size(); i++){
+            cout<<nums[i]<<" ";
+        }
+        cout<<endl;
+        for(int i=1; i<nums.size(); i++){
+            if(nums[i]==nums[i-1]){
+                res.push_back(nums[i]);
+            }
+        }
+        return res;
+    }
+};
