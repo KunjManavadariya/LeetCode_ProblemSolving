20d03ff61c09734c62af1f2dd285a1a623a4e6c0
Kunj Manavadariya <manavadariyakunj@gmail.com>
2022-08-02 21:29:53 +0530

    Time: 489 ms (7.59%), Space: 36.8 MB (5.15%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.py
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.py
@@ -1,6 +1,6 @@
 import numpy as np
 from functools import lru_cache
-@lru_cache()
+@lru_cache(maxsize=128)
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
         arr=np.array(matrix)
