3ccdfe904838eff8854c87ccb86ea6b2d751e143
Kunj Manavadariya <manavadariyakunj@gmail.com>
2022-08-17 01:49:20 +0530

    Time: 0 ms (100.00%), Space: 9 MB (68.01%) - LeetHub

--- 443-string-compression/443-string-compression.cpp
+++ 443-string-compression/443-string-compression.cpp
@@ -1,30 +1,8 @@
 class Solution {
 public:
     int compress(vector<char>& chars) {
-//         int n= chars.size();
-//         if(n < 2) return n;
-//         int count=1;
-//         string res="";
         
-//         for(int i=0; i<n; i++){
-//             res+=(chars[i]);
-//             while(i<n-1 && chars[i]==chars[i+1]){
-//                 count++;
-//                 i++;
-//             }
-//             if(count>1){
-//             res=res+to_string(count);
-//             }
-//             count=1;
-//             }
-
-//         for(int i=0; i<res.size(); i++){
-//             chars[i]=res[i];
-//         }
-        
-//         return res.size();
-        
-        chars.push_back('!');
+        // chars.push_back('!');
         int n= chars.size(), count=1;
         string s;
         
@@ -32,12 +10,15 @@ public:
         for(int i=1; i<n; i++){
             if(chars[i]==s.back()){
                 count++;
+                 if(i==n-1){
+                    s+=to_string(count);
+                }
             }
             else{
                 if(count>1){
                 s+=to_string(count);
                 }
-                if(chars[i]!='!') s.push_back(chars[i]);
+                s.push_back(chars[i]);
                 count=1;
             }
         }
