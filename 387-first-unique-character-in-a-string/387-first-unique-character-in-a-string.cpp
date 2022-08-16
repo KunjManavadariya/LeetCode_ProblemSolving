0cb1a84403696421f5f2eb7699a47c94e0997b9a
Kunj Manavadariya <manavadariyakunj@gmail.com>
2022-08-16 18:49:29 +0530

    Time: 38 ms (74.94%), Space: 10.6 MB (91.52%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        int freq[26] = {0};
-        for(int i=0; i<s.length(); i++){
-            freq[s[i]-'a']++;
+        int freq[26] = {0}, i=0;
+        while(i<s.length()){
+            freq[s[i++]-'a']++;
         }
-        for(int i=0; i<s.length(); i++){
+        i=0;
+        while(i<s.length()){
             if(freq[s[i]-'a']==1){
                 return i;
             }
+            i++;
         }
         return -1;
     }
