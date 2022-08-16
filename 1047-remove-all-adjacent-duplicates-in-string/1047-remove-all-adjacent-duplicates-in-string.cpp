690d630be50ec35ee45c4fcdaaa64b5421887aae
Kunj Manavadariya <manavadariyakunj@gmail.com>
2022-08-16 20:01:37 +0530

    Time: 18 ms (92.05%), Space: 11.1 MB (50.19%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     string removeDuplicates(string s) {
-        int i=0;
-        while(i<s.length()){
-            if(s[i]==s[i+1]){
-                s.erase(i, 2);
-                if(i-1>=0 && s[i-1]==s[i]){
-                    i-=1;
+        string res;
+        res.push_back(s[0]);
+            for(int i=1; i<s.length(); i++){
+                if(s[i]==res.back()){
+                    res.pop_back();
+                }
+                else{
+                    res.push_back(s[i]);
                 }
             }
-            else{
-                i++;
-            }
-            
-        }
-        return s;
+        return res;
     }
 };
