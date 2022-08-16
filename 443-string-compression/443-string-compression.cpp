f26a99d358e19ad2ded9b90f6b92d1ca49c623a8
Kunj Manavadariya <manavadariyakunj@gmail.com>
2022-08-17 01:41:59 +0530

    Time: 7 ms (67.48%), Space: 9 MB (68.01%) - LeetHub

--- 443-string-compression/443-string-compression.cpp
+++ 443-string-compression/443-string-compression.cpp
@@ -1,27 +1,49 @@
 class Solution {
 public:
     int compress(vector<char>& chars) {
-        int n= chars.size();
-        if(n < 2) return n;
-        int count=1;
-        string res="";
+//         int n= chars.size();
+//         if(n < 2) return n;
+//         int count=1;
+//         string res="";
         
-        for(int i=0; i<n; i++){
-            res+=(chars[i]);
-            while(i<n-1 && chars[i]==chars[i+1]){
-                count++;
-                i++;
-            }
-            if(count>1){
-            res=res+to_string(count);
-            }
-            count=1;
-            }
+//         for(int i=0; i<n; i++){
+//             res+=(chars[i]);
+//             while(i<n-1 && chars[i]==chars[i+1]){
+//                 count++;
+//                 i++;
+//             }
+//             if(count>1){
+//             res=res+to_string(count);
+//             }
+//             count=1;
+//             }
 
-        for(int i=0; i<res.size(); i++){
-            chars[i]=res[i];
-        }
+//         for(int i=0; i<res.size(); i++){
+//             chars[i]=res[i];
+//         }
         
-        return res.size();
+//         return res.size();
+        
+        chars.push_back('!');
+        int n= chars.size(), count=1;
+        string s;
+        
+        s.push_back(chars[0]);
+        for(int i=1; i<n; i++){
+            if(chars[i]==s.back()){
+                count++;
+            }
+            else{
+                if(count>1){
+                s+=to_string(count);
+                }
+                if(chars[i]!='!') s.push_back(chars[i]);
+                count=1;
+            }
+        }
+        for(int i=0; i<s.size(); i++){
+            chars[i]=s[i];
+        }
+        return s.size();
     }
 };
