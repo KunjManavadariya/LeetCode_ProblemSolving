f1ba2a6a19738329ae05343a8a44eb7ab1a79348
Kunj Manavadariya <manavadariyakunj@gmail.com>
2022-08-16 20:07:18 +0530

    Time: 28 ms (73.88%), Space: 11 MB (63.84%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -4,9 +4,7 @@ public:
         string res;
         res.push_back(s[0]);
             for(int i=1; i<s.length(); i++){
-                if(s[i]==res.back()){
-                    res.pop_back();
-                }
+                if(s[i]==res.back()) res.pop_back();
                 else res.push_back(s[i]);
             }
         return res;
