21bfadb9d05f17a1b5956b3cb592bd34f0437c52
Kunj Manavadariya <manavadariyakunj@gmail.com>
2022-08-17 19:07:47 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (90.53%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        if(needle.empty()){
-            return 0;
-        }
+        if(needle.empty()) return 0;
        return haystack.find(needle); 
     }
 };
