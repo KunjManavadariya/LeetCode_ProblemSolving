38cd537546e16928c01fbb436964179af9db1e3a
Kunj Manavadariya <manavadariyakunj@gmail.com>
2022-08-22 00:08:09 +0530

    Time: 0 ms (100.00%), Space: 6 MB (25.59%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -3,14 +3,13 @@ public:
     int mySqrt(int x) {
         if (x==0) return 0;
         int start = 1, end = INT_MAX;
-        while(start<=end){
+        while(1){
             int mid = start + (end - start)/2;
             if (mid > x/mid) end = mid - 1;
             else {
-                // if (mid + 1 > x/(mid + 1)) return mid;
+                if (mid + 1 > x/(mid + 1)) return mid;
                 start = mid + 1;
             }
         }
-        return end;
     }
 };
