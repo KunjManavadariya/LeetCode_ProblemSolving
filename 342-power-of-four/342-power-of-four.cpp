8049d3e35c8c8657b6984d26b5f558d627cd47a4
Kunj Manavadariya <manavadariyakunj@gmail.com>
2022-08-22 09:14:15 +0530

    Time: 3 ms (58.67%), Space: 6.1 MB (6.02%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -5,12 +5,10 @@ public:
             return false;
         }
         else{
-            int i=0;
-            while(pow(4,i)<=n){
+            for(int i=0; i<=sqrt(n)/2; i++){
                 if(pow(4,i)==n){
                     return true;
                 }
-                i++;
             }
             return false;
         }
