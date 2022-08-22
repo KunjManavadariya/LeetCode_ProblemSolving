e21c9246668b3934f3a9e39d260021811be9a329
Kunj Manavadariya <manavadariyakunj@gmail.com>
2022-08-22 10:08:02 +0530

    Time: 12 ms (17.66%), Space: 9.7 MB (22.77%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.cpp
+++ 35-search-insert-position/35-search-insert-position.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int n=nums.size();
-        for(int i=0; i<n; i++){
+        for(int i=0; i<nums.size(); i++){
             if(nums[i]==target) return i;
             if(nums[i]>target) return i;
         }
-        return n;
+        return nums.size();
     }
 };
