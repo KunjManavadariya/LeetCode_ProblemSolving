b1b9434891e63e19704639ce450bf6160fce15d1
Kunj Manavadariya <manavadariyakunj@gmail.com>
2022-08-24 22:26:16 +0530

    Time: 4 ms (39.73%), Space: 6.5 MB (88.89%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.cpp
+++ 58-length-of-last-word/58-length-of-last-word.cpp
@@ -1,31 +1,31 @@
-// class Solution {
-// public:
-//     int lengthOfLastWord(string s) {
-//         int i=s.size()-1, res=0;
-//         while(s[i]==' ') i--;
-//         while(i>=0 && s[i--]!=' '){
-//             res++;
-//         }
-//         return res;
-//     }
-// };
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int l = s.size();
-        if (l==0) {
-            return 0;
+        int i=s.size()-1, res=0;
+        while(s[i]==' ') i--;
+        while(i>=0 && s[i--]!=' '){
+            res++;
         }
-        int sl = 0;
-        for (int i=l-1;i>=0;i--) {
-            if (s[i] == ' ' && sl > 0) {
-                return sl;
-            } else if(s[i] != ' ') {
-                sl++;
-            }
-            cout<<sl<<" ";
-        }
-        cout<<endl;
-        return sl;
+        return res;
     }
 };
+// class Solution {
+// public:
+//     int lengthOfLastWord(string s) {
+//         int l = s.size();
+//         if (l==0) {
+//             return 0;
+//         }
+//         int sl = 0;
+//         for (int i=l-1;i>=0;i--) {
+//             if (s[i] == ' ' && sl > 0) {
+//                 return sl;
+//             } else if(s[i] != ' ') {
+//                 sl++;
+//             }
+//             cout<<sl<<" ";
+//         }
+//         cout<<endl;
+//         return sl;
+//     }
+// };
