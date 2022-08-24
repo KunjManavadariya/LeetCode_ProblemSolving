1bca691caf046a45fef37c7338e43b7ae1cda7d9
Kunj Manavadariya <manavadariyakunj@gmail.com>
2022-08-24 20:38:27 +0530

    Time: 35 ms (49.24%), Space: 5.9 MB (29.00%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     bool isPowerOfThree(int n) {
         if(n<=0) return false;
-        return 1162261467%n==0;
+        int power = pow(3,19);
+        int res=power%n;
+        return res==0;
     }
 };
