bd32a86c0d856a42ec6416b6f626277b0a5a5045
Kunj Manavadariya <manavadariyakunj@gmail.com>
2022-08-24 22:09:35 +0530

    Time: 5 ms (27.88%), Space: 6.5 MB (51.75%) - LeetHub

---
+++ 58-length-of-last-word/58-length-of-last-word.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int i=s.size()-1, res=0;
+        while(s[i]==' ') i--;
+        while(i>=0 && s[i--]!=' '){
+            res++;
+        }
+        return res;
+    }
+};
