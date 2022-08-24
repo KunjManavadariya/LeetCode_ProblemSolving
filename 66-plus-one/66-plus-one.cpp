e1b5a194f1895df76b5e9f47e730bb9f2d8a1231
Kunj Manavadariya <manavadariyakunj@gmail.com>
2022-08-24 23:48:39 +0530

    Time: 8 ms (12.36%), Space: 8.7 MB (85.87%) - LeetHub

---
+++ 66-plus-one/66-plus-one.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n=digits.size();
+        if(digits[n-1]!=9){
+            digits[n-1]++;
+        }
+        else{
+            digits[n-1]=0;
+            for(int i=n-2; i>=0; i--){
+                if(digits[i]==9 && i-1>=0){
+                    digits[i]=0;
+                }
+                else if(digits[i]!=9){
+                    digits[i]+=1;
+                    return digits;
+                }
+            }
+             digits[0]=0;
+            digits.insert(digits.begin(), 1);
+        }
+        return digits;
+    }
+};
