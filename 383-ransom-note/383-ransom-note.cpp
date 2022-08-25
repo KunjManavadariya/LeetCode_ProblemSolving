3a025cddd75a99eb035d6a8284f810689b8e8490
Kunj Manavadariya <manavadariyakunj@gmail.com>
2022-08-25 18:56:36 +0530

    Time: 12 ms (91.71%), Space: 8.8 MB (72.69%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int freq[26] = {0};
+        for(int i=0; i<magazine.size(); i++){
+            freq[magazine[i]-'a']++;
+        }
+        for(int i=0; i<ransomNote.size(); i++){
+            if(freq[ransomNote[i]-'a']==0) return false;
+            freq[ransomNote[i]-'a']--;
+        }
+        return true;
+    }
+};
