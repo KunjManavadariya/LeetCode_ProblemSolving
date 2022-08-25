55db0176c1c5fae4f5059588afffbe8af8ecc641
Kunj Manavadariya <manavadariyakunj@gmail.com>
2022-08-25 19:38:13 +0530

    Time: 63 ms (7.74%), Space: 8.8 MB (72.69%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -1,14 +1,41 @@
+// class Solution {
+// public:
+//     bool canConstruct(string ransomNote, string magazine) {
+//         int freq[26] = {0};
+//         for(int i=0; i<magazine.size(); i++){
+//             freq[magazine[i]-'a']++;
+//         }
+//         for(int i=0; i<ransomNote.size(); i++){
+//             if((freq[ransomNote[i]-'a']--)==0) return false;
+//         }
+//         return true;
+//     }
+// };
+
 class Solution {
 public:
-    bool canConstruct(string ransomNote, string magazine) {
-        int freq[26] = {0};
-        for(int i=0; i<magazine.size(); i++){
-            freq[magazine[i]-'a']++;
-        }
-        for(int i=0; i<ransomNote.size(); i++){
-            if((freq[ransomNote[i]-'a']--)==0) return false;
-            // freq[ransomNote[i]-'a']--;
-        }
-        return true;
+bool canConstruct(string ransomNote, string magazine) {
+int count=0;
+for(int i=0;i<ransomNote.size();i++)
+{
+    for(int j=0;j<magazine.size();j++)
+    {
+        if(ransomNote[i]==magazine[j])
+            {
+            count++;
+            magazine[j]=-1;
+            break;
+            }
+        if(count==ransomNote.size())
+            {
+            return true;
+            }
     }
+}
+if(count==ransomNote.size())
+            {
+            return true;
+            }
+return false;
+}
 };
