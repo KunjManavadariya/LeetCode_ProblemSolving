ffd9857892ac6b765be5388611640a1b3a04a441
Kunj Manavadariya <manavadariyakunj@gmail.com>
2022-08-26 02:22:01 +0530

    Time: 32 ms (12.97%), Space: 8.3 MB (33.31%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.cpp
+++ 12-integer-to-roman/12-integer-to-roman.cpp
@@ -11,20 +11,20 @@ public:
             if(num<1000){
                 for(int i=0; i<13; i++){
                     if(num<val[i]){ 
-                        res = res+letter[i-1];
+                        res += letter[i-1];
                         num-=val[i-1];
                         break;
                     }
                 }
                 for(int i=0; i<13; i++){
                     if(num==val[i]){
-                        res = res+letter[i];
+                        res += letter[i];
                         return res;
                     }
                 }
             }
             else{
-                res = res+letter[12];
+                res += letter[12];
                 num-=1000;
             }
         }
