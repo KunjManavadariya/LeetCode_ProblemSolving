b09d19749d48e87b308c5fb5d7084d95ad4c84e9
Kunj Manavadariya <manavadariyakunj@gmail.com>
2022-08-26 10:04:31 +0530

    Time: 16 ms (41.91%), Space: 8.3 MB (33.31%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.cpp
+++ 12-integer-to-roman/12-integer-to-roman.cpp
@@ -4,11 +4,13 @@ public:
         int val[13] = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
         string res = "";
         vector<string> letter = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
-        for(int i=0; i<13; i++) if(num==val[i]) return letter[i];
+        for(int i=0; i<13; i++){
+            if(num==val[i]) return letter[i];
+        }
         while(num>0){
             if(num<1000){
                 int i=0;
-                for(i;num>0 && i<13; i++){
+                while(num>0 && i<13){
                             if(num<val[i]){ 
                                 res += letter[i-1];
                                 num-=val[i-1];
@@ -18,6 +20,7 @@ public:
                                 res += letter[i];
                                 return res;
                             }
+                    i++;
                 }
             }
 
