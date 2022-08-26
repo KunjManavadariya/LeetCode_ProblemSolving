4f0319351357509443b0cf665e8d744c3e81d6e5
Kunj Manavadariya <manavadariyakunj@gmail.com>
2022-08-26 12:14:09 +0530

    Time: 87 ms (25.93%), Space: 27.8 MB (36.51%) - LeetHub

--- 628-maximum-product-of-three-numbers/628-maximum-product-of-three-numbers.cpp
+++ 628-maximum-product-of-three-numbers/628-maximum-product-of-three-numbers.cpp
@@ -4,10 +4,7 @@ public:
         sort(nums.begin(), nums.end());
         int n=nums.size()-1;
         if(nums[0]<0 && nums[1]<0 && nums[n]>0){
-            if((nums[0]*nums[1])>(nums[n-1]*nums[n-2])){
-            int res = nums[n]*nums[0]*nums[1];
-            return res;
-            }
+            if((nums[0]*nums[1])>(nums[n-1]*nums[n-2])) return nums[n]*nums[0]*nums[1];
         }
         return nums[n]*nums[n-1]*nums[n-2];
     }
