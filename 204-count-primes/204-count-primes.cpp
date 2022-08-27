40e53a41d0fd492dd0eb35d8c6a25cb06fe87d98
Kunj Manavadariya <manavadariyakunj@gmail.com>
2022-08-27 19:09:24 +0530

    Time: 284 ms (62.28%), Space: 10.3 MB (70.74%) - LeetHub

--- 204-count-primes/204-count-primes.cpp
+++ 204-count-primes/204-count-primes.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
-    int countPrimes(int n) {
-        //Sieve of Eratosthenes
+    int countPrimes(int n) {                                     //Sieve of Eratosthenes
         int count=0;
         if(n==0 || n==1) return 0;
-        
-        vector<bool> arr(n+1, 1);
-        
+        vector<bool> vec(n+1, 1);                                //One size more in vector just to be safe from overflow
         for(int i=2; i<n; i++){
-            if(arr[i]!=0){
+            if(vec[i]!=0){
                 count++;
-                for(int j=i*2; j<n; j+=i) arr[j]=0;
+                for(int j=i*2; j<n; j+=i) vec[j]=0;             //If the element is not marked 0, then make all its multiples 0 till n as they won't be prime.
             }
         }
-        
     return count;
     }
 };
