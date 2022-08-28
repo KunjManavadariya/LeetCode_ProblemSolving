bc8dfc45d6b1f7cc909fc74a5a89261146bffd49
Kunj Manavadariya <manavadariyakunj@gmail.com>
2022-08-28 19:09:45 +0530

    Time: 41 ms (6.35%), Space: 8.7 MB (88.57%) - LeetHub

---
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
+       int count=0, row=mat.size(), col=mat[0].size(); 
+      while(count<row){
+          for(int i=1; i<row; i++){
+              for(int j=1; j<col; j++){
+                  if(mat[i][j]<mat[i-1][j-1]){
+                      swap(mat[i][j], mat[i-1][j-1]);
+                  }
+              }
+          }
+          count++;
+      }
+        return mat;
+    }
+};
