83d6fe19dfaaecd465b4068aac694dadd2fbe039
Kunj Manavadariya <manavadariyakunj@gmail.com>
2022-08-28 19:14:00 +0530

    Time: 17 ms (57.89%), Space: 9.7 MB (7.49%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -1,15 +1,34 @@
+// class Solution {
+// public:
+//     vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
+//        int count=0, row=mat.size(), col=mat[0].size(); 
+//       while(count<row){
+//           for(int i=1; i<row; i++){
+//               for(int j=1; j<col; j++){
+//                   if(mat[i][j]<mat[i-1][j-1]) swap(mat[i][j], mat[i-1][j-1]);
+//               }
+//           }
+//           count++;
+//       }
+//         return mat;
+//     }
+// };
 class Solution {
 public:
     vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
-       int count=0, row=mat.size(), col=mat[0].size(); 
-      while(count<row){
-          for(int i=1; i<row; i++){
-              for(int j=1; j<col; j++){
-                  if(mat[i][j]<mat[i-1][j-1]) swap(mat[i][j], mat[i-1][j-1]);
-              }
-          }
-          count++;
-      }
+        int m = mat.size(), n = mat[0].size();
+        map<int, priority_queue<int, vector<int>, greater<int>>> d;
+        for(int i=0; i < m; i++){
+            for(int j=0; j < n; j++){
+                d[i - j].push(mat[i][j]);
+            }
+        }
+        for(int i=0; i < m; i++){
+            for(int j=0; j < n; j++){
+                mat[i][j] = d[i - j].top();
+                d[i - j].pop();
+            }
+        }
         return mat;
     }
 };
