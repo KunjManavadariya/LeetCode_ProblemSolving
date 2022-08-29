b656ac515086e80a22fe100163bcf66cbb9b927d
Kunj Manavadariya <manavadariyakunj@gmail.com>
2022-08-29 12:25:23 +0530

    Time: 12 ms (36.08%), Space: 5.8 MB (96.11%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -3,7 +3,7 @@ public:
     int fibo(int n){
         int ans=0;
         if(n==0 || n==1) return 1;
-        ans+=fib(n-1)+fib(n-2);
+        ans=fib(n-1)+fib(n-2);
         return ans;
     }
     
