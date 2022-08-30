663d2e4d27991b53eea2a49b9606bf9d1acdb89a
Kunj Manavadariya <manavadariyakunj@gmail.com>
2022-08-31 01:52:49 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (57.87%) - LeetHub

--- 67-add-binary/67-add-binary.cpp
+++ 67-add-binary/67-add-binary.cpp
@@ -3,6 +3,7 @@ public:
     string addBinary(string a, string b) {
         reverse(a.begin(), a.end());
         reverse(b.begin(), b.end());
+        
         if(a.size()>b.size()){
             for(int i=b.size(); i<a.size(); i++){
                 b.push_back('0');
@@ -17,27 +18,17 @@ public:
         string res;
         char carry='0';
         for(int i=0; i<a.size(); i++){
-            if(a[i]=='1'&&b[i]=='1'&&carry=='0'){
-                res.push_back('0');
+            if(a[i]=='1'&&b[i]=='1'){
+                res.push_back(carry);
                 carry='1';
             }
-            else if(a[i]=='1'&&b[i]=='1'&&carry=='1'){
-                res.push_back('1');
-                carry='1';
+            else if((a[i]=='1' || b[i]=='1')){
+                if(carry=='1') res.push_back('0');
+                else res.push_back('1');
             }
-            else if((a[i]=='1' || b[i]=='1') && carry=='1'){
-                res.push_back('0');
-                carry='1';
-            }
-            else if((a[i]=='1' || b[i]=='1') && carry=='0'){
-                res.push_back('1');
-            }
-            else if((a[i]=='0' && b[i]=='0') && carry=='0'){
-                res.push_back('0');
-            }
-            else if((a[i]=='0' && b[i]=='0') && carry=='1'){
-                res.push_back('1');
-                carry='0';
+            else{
+                res.push_back(carry);
+                if(carry=='1') carry='0';
             }
             
         }
