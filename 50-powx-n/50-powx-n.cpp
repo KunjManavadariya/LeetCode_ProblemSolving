69fae499cfee9c71378051831e04647d9c571ef5
Kunj Manavadariya <manavadariyakunj@gmail.com>
2022-09-01 23:40:57 +0530

    Time: 2 ms (54.54%), Space: 5.9 MB (46.18%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        // if(n==0) return 1;
-        // if(n<0){
-        //     n=abs(n);
-        //     x=1/x;
-        // }
-        // if(n%2!=0) return x*myPow(x*x, n>>=1);
-        // else return myPow(x*x, n>>=1);
         double ans=  1;
         while(n){
             if(n<0){
