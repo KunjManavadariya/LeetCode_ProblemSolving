a3ce0afb61b5ab902f3bb2d5012e6c0f127b0908
Kunj Manavadariya <manavadariyakunj@gmail.com>
2022-09-01 23:13:38 +0530

    Time: 4 ms (33.94%), Space: 5.9 MB (81.48%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -6,7 +6,7 @@ public:
             n=abs(n);
             x=1/x;
         }
-        if(n%2!=0) return x*myPow(x*x, n/2);
-        else return myPow(x*x, n/2);
+        if(n%2!=0) return x*myPow(x*x, n>>=1);
+        else return myPow(x*x, n>>=1);
     }
 };
