10b0ea4fe0c75277c0d571e7528e58d2fecf1a50
Kunj Manavadariya <manavadariyakunj@gmail.com>
2022-09-07 00:43:51 +0530

    Time: 10 ms (57.75%), Space: 15 MB (5.63%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cpp
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -9,27 +9,19 @@ public:
             cout<<freq[i]<<" ";
         }
         cout<<endl;
-        int temp = nums1[0];
         for(int i=0; i<nums1.size(); i++){
-            temp = nums1[i];
             bool  p = false;
-            // if(freq[nums1[i]!=-1]){
-                for(int j = freq[nums1[i]]; j<nums2.size(); j++){
+                for(int j = freq[nums1[i]]+1; j<nums2.size(); j++){
                     if(nums2[j]>nums1[i]){
                         p = true;
                         nums1[i] = nums2[j];
                         break;
                     }
                 }
-            // }
             if(!p){
                 nums1[i] = -1;
             }
         }
-        // cout<<temp<<endl;
-        // if(temp == nums1[nums1.size()-1]){
-        //     nums1[nums1.size()-1] = -1;
-        // }
         return nums1;
     }
 };
