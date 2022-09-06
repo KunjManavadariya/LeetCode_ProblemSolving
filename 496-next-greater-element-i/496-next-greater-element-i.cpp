3c0c01894f67dd03664f5a30978db58ecf34b160
Kunj Manavadariya <manavadariyakunj@gmail.com>
2022-09-07 00:40:04 +0530

    Time: 4 ms (91.04%), Space: 15.2 MB (5.63%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cpp
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -1,35 +1,35 @@
-// class Solution {
-// public:
-// //     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-// //        for(int i=0, j=0; i<nums2.size(); i++){
-// //            if(nums1[i]==nums2[j]){
-// //                for(int k=j; k<nums2.size(); k++){
-                   
-// //                }
-// //            }
-// //             }
-// //        }
-
-//     }
-// };
-// OJ: https://leetcode.com/problems/next-greater-element-i/
-// Author: github.com/lzl124631x
-// Time: O(N)
-// Space: O(N)
 class Solution {
 public:
-    vector<int> nextGreaterElement(vector<int>& findNums, vector<int>& nums) {
-        stack<int> s;
-        unordered_map<int, int> m;
-        for (int n : nums) {
-            while (s.size() && s.top() < n) {
-                m[s.top()] = n;
-                s.pop();
-            }
-            s.push(n);
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+       vector<int> freq(100002, -1);
+        for(int i=0; i<nums2.size(); i++){
+            freq[nums2[i]] = i;
         }
-        vector<int> ans;
-        for (int n : findNums) ans.push_back(m.count(n) ? m[n] : -1);
-        return ans;
+        for(int i=0; i<10; i++){
+            cout<<freq[i]<<" ";
+        }
+        cout<<endl;
+        int temp = nums1[0];
+        for(int i=0; i<nums1.size(); i++){
+            temp = nums1[i];
+            bool  p = false;
+            // if(freq[nums1[i]!=-1]){
+                for(int j = freq[nums1[i]]; j<nums2.size(); j++){
+                    if(nums2[j]>nums1[i]){
+                        p = true;
+                        nums1[i] = nums2[j];
+                        break;
+                    }
+                }
+            // }
+            if(!p){
+                nums1[i] = -1;
+            }
+        }
+        // cout<<temp<<endl;
+        // if(temp == nums1[nums1.size()-1]){
+        //     nums1[nums1.size()-1] = -1;
+        // }
+        return nums1;
     }
 };
