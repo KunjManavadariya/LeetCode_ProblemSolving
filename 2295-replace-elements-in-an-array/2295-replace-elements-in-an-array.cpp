262a23e773fbe0da3d976f3649942f94f120524e
Kunj Manavadariya <manavadariyakunj@gmail.com>
2022-09-07 20:24:14 +0530

    Time: 700 ms (65.21%), Space: 111.2 MB (97.30%) - LeetHub

--- 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.cpp
+++ 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     vector<int> arrayChange(vector<int>& nums, vector<vector<int>>& operations) {
-        int freq[1000002] = {-1};
-        for(int i=0; i<nums.size(); i++){
-            freq[nums[i]]=i;
+        int pos[1000002] = {-1};
+        for(int i=0; i<nums.size(); i++){                //Loop to store 
+            pos[nums[i]]=i;
         }
         for(int i=0; i<operations.size(); i++){
-            if(freq[operations[i][0]] != -1){
-                nums[freq[operations[i][0]]]=operations[i][1];
-                freq[nums[freq[operations[i][0]]]]=-1;
-                freq[operations[i][1]]=freq[operations[i][0]];
+            if(pos[operations[i][0]] != -1){
+                nums[pos[operations[i][0]]]=operations[i][1];
+                pos[operations[i][1]]=pos[operations[i][0]];
+                pos[operations[i][0]]=-1;
             }
         }
         return nums;
