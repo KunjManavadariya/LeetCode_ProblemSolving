32e18e3fb092f72c1ccbc63600e2d1a0e2b61c92
Kunj Manavadariya <manavadariyakunj@gmail.com>
2022-09-08 19:23:07 +0530

    Time: 16 ms (63.88%), Space: 10.3 MB (48.61%) - LeetHub

--- 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.cpp
+++ 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.cpp
@@ -4,15 +4,11 @@ public:
         int count=0;
         bool flag=0;
         for(int i=0; i<words.size(); i++){
-            if(words[i][0]==pref[0] && words[i].size()>=pref.size()){
-                for(int j=1; j<pref.length(); j++){
-                    if(words[i][j]!=pref[j]) flag=1;
-                }
-                if(flag==1){ 
-                    flag=0;
-                    continue;
-                }
-                else count++;
+            words[i] = words[i].substr(0, pref.size());
+        }
+        for(int i=0; i<words.size(); i++){
+            if(words[i]==pref){
+                count++;
             }
         }
         return count;
